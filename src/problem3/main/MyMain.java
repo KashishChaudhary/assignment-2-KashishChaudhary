dc400678cd7a300122f94ae6f1cd8d835973dcf2
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 16:35:33 +0530

    object created

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,15 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+
+
+    }
 }
