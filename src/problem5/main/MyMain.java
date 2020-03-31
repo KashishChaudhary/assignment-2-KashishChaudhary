2fbf51a75acda18f378365ccc3b3ef3522f5cdb6
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 17:48:47 +0530

    values stored in queue

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -1,10 +1,24 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
+
 package problem5.main;
+
+import problem5.student.Student;
+
+import java.util.ArrayList;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(new Student(1, "Abc", 1, 0));
+        studentList.add(new Student(2, "nbh", 2, 2));
+        studentList.add(new Student(3, "dgf", 3, 1));
+        studentList.add(new Student(4, "hjf", 8, 3));
+        studentList.add(new Student(5, "E", 0, 0));
+        studentList.add(new Student(6, "hdhf", 2, 1));
+        studentList.add(new Student(7, "dhf", 5, 3));
+        studentList.add(new Student(8, "qwe", 7, 5));
+        studentList.add(new Student(9, "Irt", 9, 6));
+        studentList.add(new Student(10, "Jlki", 0, 0));
+        studentList.add(new Student(11, "chaudhary", 2, 1));
+    }
 }
