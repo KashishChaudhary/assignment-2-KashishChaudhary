9af99d93b3a1094926af47903e55c314885bf541
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 17:23:50 +0530

    variables created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,11 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
+
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    private Student student;
+    private Node next;
+
 }
