08b2895d4120b76566c54b5c9ee3fec58eb7ac0d
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 17:12:54 +0530

    constructer is created

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,7 +6,10 @@ public class Student {
     private String Name;
     private int backLogCounter;
     private int appearingCounter;
-
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        Name = name;
+    }
 
     public boolean getRollNumber() {
     }
