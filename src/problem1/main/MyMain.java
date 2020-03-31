d1d7a4b1ad92b8493b3d45732df9aae71797fe60
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 15:54:50 +0530

    input is taken

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,26 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
+
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree t = new MyBinarySearchTree();
+        Scanner sc = new Scanner(System.in);
+        int i = 7;
+        System.out.println("Please take only 7 inputs");
+        while (i > 0) {
+            try {
+                t.add(sc.nextInt());
+            } catch (Exception e) {
+                System.out.println("Error Occurred is \" " + e + " \", so you are going to be kicked-out \n");
+                break;
+            }
+            i--;
+        }
+
+    }
 }
