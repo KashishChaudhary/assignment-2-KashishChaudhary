7c54965b9078f63e86a1441166ac1fb1cc227b88
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 17:12:16 +0530

    veriables created

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,12 +1,13 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
+
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int rollNumber;
+    private String Name;
+    private int backLogCounter;
+    private int appearingCounter;
+
+
     public boolean getRollNumber() {
     }
 }
