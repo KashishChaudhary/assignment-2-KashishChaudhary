9d3de3de365b0cdcf5826d22afe354a562b0c9fe
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 17:24:25 +0530

    constructer is created

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,5 +7,8 @@ import problem5.student.Student;
 public class Node {
     private Student student;
     private Node next;
-
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
 }
