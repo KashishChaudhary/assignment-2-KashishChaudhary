5ebcb7752f59f9019cce38842a6608f01010ebad
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 15:27:29 +0530

    create constructor

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,16 +1,15 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
-package problem1.mybst;
 
+package problem1.mybst;
 import problem1.node.TreeNode;
 
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     public static StringBuilder preOrderString;
     public TreeNode data;
+    public int count=0;
+    public MyBinarySearchTree{
+        this.data = null;
+        preOrderString = new StringBuilder();
+    }
 
 }
