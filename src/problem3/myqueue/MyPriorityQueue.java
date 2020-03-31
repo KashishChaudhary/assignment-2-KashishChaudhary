62e9e6f6c5c783d87ff3b8a47788260747906d77
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 16:33:11 +0530

    print the queue

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -51,5 +51,16 @@ public class MyPriorityQueue {
             }
         }
     }
+    public void printQueue() {
+        Node temp = this.front;
+        for (int i = 0; i < this.size; i++) {
+            if (i != this.size - 1) {
+                System.out.print(temp.getData().toString() + "--->");
+                temp = temp.getNext();
+            } else {
+                System.out.println(temp.getData().toString());
+            }
+        }
+    }
 }
 
