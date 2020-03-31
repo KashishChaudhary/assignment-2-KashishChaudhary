d094ecef6b13bfa41f75c3bc11f0efbcd921e82b
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 16:00:59 +0530

    input has been taken

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -1,11 +1,27 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
- */
+
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+//use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree t = new MyBinarySearchTree();
+        Scanner s = new Scanner(System.in);
+        int i = 7;
+        System.out.println("Please take only 7 inputs ...");
+        while (i > 0) {
+            try {
+                t.add(s.nextInt());
+            } catch (Exception x) {
+                System.out.println("You Entered invalid no. ... you are kicked :) ");
+                break;
+            }
+            i--;
+        }
+
+    }
 }
