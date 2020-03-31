3a188885d60ec2bf8e1ba62d01873efbf108ef07
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 16:59:42 +0530

    getter ans setter method created

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,10 +8,12 @@ import problem3.node.Node;
 public class MyQueue {
     private Node front;
     private Node rear;
+
     public MyQueue() {
         front = null;
         rear = null;
     }
+
     public void enqueue(int data) {
         Node ns = new Node();
         ns.setData(data);
@@ -22,6 +24,7 @@ public class MyQueue {
             rear = ns;
         }
     }
+
     public void preSuccessor(TreeNode root) {
         if (root == null)
             return;
@@ -31,6 +34,7 @@ public class MyQueue {
             preSuccessor(root.getRight());
         }
     }
+
     public void getPreSucc() {
         Node temp = front.getNext();
         while (temp != null) {
@@ -38,9 +42,25 @@ public class MyQueue {
             temp = temp.getNext();
         }
     }
+
     static class Node {
         int data;
         Node next;
 
+        public int getData() {
+            return data;
+        }
 
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
+}
