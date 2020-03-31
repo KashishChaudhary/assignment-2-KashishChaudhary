03be175c5b63a5ccc2f9be0160f4701f76b21f12
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 17:03:56 +0530

    enter elements to add

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,11 +1,18 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:08 PM
- */
+
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree t = new MyBinarySearchTree();
+        t.add(55);
+        t.add(98);
+        t.add(63);
+        t.add(45);
+        t.add(25);
+
+    }
 }
