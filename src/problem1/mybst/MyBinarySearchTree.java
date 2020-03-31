febc9c9b7ea4e8a9e3a90daa13e835f37167553e
Kashish Chaudhary <kashish.chaudhary_cs18@gla.ac.in>
2020-03-31 15:35:20 +0530

    create object preOrder

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,10 +6,17 @@ import problem1.node.TreeNode;
 public class MyBinarySearchTree {
     public static StringBuilder preOrderString;
     public TreeNode data;
-    public int count=0;
-    public MyBinarySearchTree{
+    private int count=0;
+    public MyBinarySearchTree(){
         this.data = null;
         preOrderString = new StringBuilder();
     }
-
+    public static void preOrder(TreeNode data) {
+        if (data == null) {
+            return;
+        }
+        preOrder(data.getLeft());
+        preOrder(data.getRight());
+        preOrderString.append(data.getData()).append(" ");
+    }
 }
